41d471cdac3f451f2ed3e35dc20911f46330aa00
Sergey Dubovyk <dubovyk@ucu.edu.ua>
2017-08-05 04:33:43 +0300

    updated page info DAO

--- src/java/com/corenlpanalyzer/api/Domain/RawPageData.java
+++ src/java/com/corenlpanalyzer/api/Domain/RawPageData.java
@@ -1,5 +1,6 @@
 package com.corenlpanalyzer.api.Domain;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -9,8 +10,92 @@ import java.util.Map;
  */
 public class RawPageData {
     private String url;
-    private int status;
+    private String status;
+    private String bodyText;
+    private String title;
+    private String titleImageLink;
+    private Map<String, String> metadata;
+    private Map<String, String> headers;
+    private String website;
+    private List links;
 
-    private Map<String, String> result;
+    public String getUrl() {
+        return url;
+    }
 
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getBodyText() {
+        return bodyText;
+    }
+
+    public void setBodyText(String bodyText) {
+        this.bodyText = bodyText;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitleImageLink() {
+        return titleImageLink;
+    }
+
+    public void setTitleImageLink(String titleImageLink) {
+        this.titleImageLink = titleImageLink;
+    }
+
+    public Map<String, String> getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Map<String, String> metadata) {
+        this.metadata = metadata;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public List getLinks() {
+        return links;
+    }
+
+    public void setLinks(List links) {
+        this.links = links;
+    }
+
+    public void addMetadata(String key, String value){
+        metadata.put(key, value);
+    }
+
+    public void addHeader(String key, String value){
+        headers.put(key, value);
+    }
 }
