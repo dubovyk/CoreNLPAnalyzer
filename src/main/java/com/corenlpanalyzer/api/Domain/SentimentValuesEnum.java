b165940a36b6f4152f37a71e155b6c5c2514e440
Sergey Dubovyk <dubovyk@ucu.edu.ua>
2017-08-13 00:55:18 +0300

    - webUI updates - added enum for sentiment possible values

---
+++ src/main/java/com/corenlpanalyzer/api/Domain/SentimentValuesEnum.java
@@ -0,0 +1,27 @@
+package com.corenlpanalyzer.api.Domain;
+
+public enum SentimentValuesEnum {
+    VERY_NEGATIVE,
+    NEGATIVE,
+    NEUTRAL,
+    POSITIVE,
+    VERY_POSITIVE;
+
+    public int getValue(){
+        switch (this){
+            case VERY_NEGATIVE:
+                return 1;
+            case NEGATIVE:
+                return 2;
+            case NEUTRAL:
+                return 3;
+            case POSITIVE:
+                return 4;
+            case VERY_POSITIVE:
+                return 5;
+            default:
+                return 0;
+        }
+    }
+
+}
